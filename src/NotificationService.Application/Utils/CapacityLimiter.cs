846f0d680f2dfa59abd912efe864ac5dde99c226
Daniel R <danieliusraudo@gmail.com>
2026-02-24 17:59:38 +0200

    add capacity limiter

---
+++ src/NotificationService.Application/Utils/CapacityLimiter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotificationService.Application.Utils;
+
+// Similar to semaphore slim with 0 timeout, the difference is changeable capacity, can lock and release more than 1
+internal class CapacityLimiter {
+    private long _capacity;
+    private long _count = 0;
+
+    public CapacityLimiter(long initialCapacity) {
+        _capacity = initialCapacity;
+    }
+
+    public long Count => Interlocked.Read(ref _count);
+    public long Capacity => Interlocked.Read(ref _capacity);
+
+    public bool TryReserve(long reserveCount) {
+        var newCount = Interlocked.Add(ref _count, reserveCount);
+
+        if(newCount > Capacity) {
+            Interlocked.Add(ref _count, reserveCount * -1);
+            return false;
+        }
+
+        return true;
+    }
+
+    public void Release(long releaseCount) {
+        Interlocked.Add(ref _count, releaseCount * -1);
+    }
+
+    public void SetCapacity(long newCapacity) {
+        Interlocked.Exchange(ref _capacity, newCapacity);
+    }
+}
--- src/NotificationService.Domain/Aggregates/Notification/NotificationStatus.cs
+++ src/NotificationService.Domain/Aggregates/Notification/NotificationStatus.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace NotificationService.Domain.Aggregates.Notification; 
 public enum NotificationStatus {
+    // sent successfully
     Sent,
+    // waiting in global retry queue
     QueuedForRetry,
+    // waiting in the provider buffer
+    QueuedForProvider,
+    // is being processed by the provider (request sent to the external provider)
+    Sending,
+    // failed permanently
     Failed,
 }
